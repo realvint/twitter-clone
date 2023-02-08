c111ac67d7040a57884404baf20a249e9c3c6257
Виктор Малухин <victor.malukhin@gmail.com>
2023-02-08 22:16:42 +0300

    Создание страницы пользователя

--- config/routes.rb
+++ config/routes.rb
@@ -13,6 +13,7 @@ Rails.application.routes.draw do
   end
 
   get :dashboard, to: 'dashboard#index'
+  get :profile, to: 'profile#show'
 
   resources :usernames, only: [:new, :update]
 end
---
+++ spec/requests/profile_spec.rb
@@ -0,0 +1,14 @@
+require 'rails_helper'
+
+RSpec.describe 'Profile', type: :request do
+  let(:user) { create(:user) }
+
+  before { sign_in user }
+
+  describe 'GET show' do
+    it 'succeeds' do
+      get profile_path
+      expect(response).to have_http_status(:success)
+    end
+  end
+end
