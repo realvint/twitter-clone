84c33222ea55d3510692ad78518a8280ce22f3d8
Виктор Малухин <victor.malukhin@gmail.com>
2023-02-09 22:17:19 +0300

    Обновление информации профиля пользователя

--- app/javascript/controllers/tweet_form_controller.js
+++ app/javascript/controllers/tweet_form_controller.js
@@ -4,7 +4,9 @@ import { Controller } from "@hotwired/stimulus"
 export default class extends Controller {
   connect() {
     this.element.addEventListener('turbo:submit-end', () => {
-      document.getElementById('close-modal-btn').click()
+      Array.from(document.getElementsByClassName('close-modal-btn')).forEach((btn) => {
+        btn.click()
+      })
       this.element.reset()
     })
   }
